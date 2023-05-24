47dc5ef8f3d707825c51dbdd1236ff166c909906
Eike09 <eike.rocha209@hotmail.com>
2023-05-24 18:51:10 -0300

    login com erro

---
+++ Codigos/usuarios.js
@@ -0,0 +1,12 @@
+let usuarioTeste = ["Eike","123.456.789-10","19123456789","eike@email.com","senha123"]
+
+/*function login(nome,senha){
+    if(nome == usuarioTeste[0] && senha == usuarioTeste[4] && nome.trim() != "" && senha.trim() != ""){
+        document.cookie = "usuario="+usuarioTeste[0];
+        document.cookie = "senha="+usuarioTeste[5];
+        console.log("aaa");
+        return true;
+    }else{
+        return false;
+    }
+}*/
